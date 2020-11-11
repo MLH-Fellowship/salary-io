baeb7bc766521e4bbace1aa03cb3cfa48f49713f
Lily Tang <lilyytang@gmail.com>
2020-11-11 14:49:10 -0800

    remove gender and years at company and change form UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home'
-import Layout1 from './components/layout/Layout1'
+import Layout1 from './components/FormFill.css'
+import FormFill from './components/FormFill'
 
 export const ThemeContext = React.createContext();
 
@@ -9,12 +10,11 @@ function App() {
 
   return (
     <div className='App'>
-      <Router>
-        <Switch>
-        <Route path="/predict" render={props =>  (<Layout1 {...props}/>)}/>
-          <Route path='/' exact component={() => <Home />} />
-        </Switch>
-      </Router>
+      <div className='wrapper'>
+        <div className='center'>
+          <FormFill />
+        </div>
+      </div>
     </div>
   );
 }
